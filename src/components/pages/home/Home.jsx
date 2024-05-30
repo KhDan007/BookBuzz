1d8107f7903fdce832e7dd08c9458b13ac7ea6a0
KhDan007 <kh.dan007@gmail.com>
2024-05-30 13:21:53 +0500

    fix margins and header and add recent search sector

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import '../../../css/home.css'
 import { Header } from "./Header"
 import { MySlider } from './MySlider'
+import { RecentSearch } from './RecentSearch'
+
+
 
 export const Home = () => {
+
+  const [isSearched, setIsSearched] = useState(true);
+  
   return <div className="home">
     <Header />
     <MySlider />
+
+    {isSearched ? <RecentSearch /> : ""}
   </div>
 }
---
+++ src/components/pages/home/RecentSearch.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import image1 from "../../../img/home/2.jpg";
+import star from "../../../img/icons/star.svg";
+
+export const RecentSearch = () => {
+  const [rating, setRating] = useState(4.5);
+
+  return (
+    <section className="recent container">
+      <h3 className="recent__subtitle">Based on Recent Search</h3>
+      <div className="flex recent__head">
+        <h2 className="recent__title">
+          #Horror Fictions, Leo Tolstoy, Marlon James
+        </h2>
+        <div className="recent__more">See more</div>
+      </div>
+      <div className="recent__items">
+        <div className="recent__elem">
+          <div className="recent__image">
+            <img src={image1} alt="" />
+          </div>
+          <div className="recent__name">To Kill A Mockingbird</div>
+          <div className="flex recent__info">
+            <div className="recent__price">Free</div>
+            <div className="flex recent__rating">
+              <div className="recent__star">
+                <img src={star} alt="" />
+              </div>
+              {rating}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
